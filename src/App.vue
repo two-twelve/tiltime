d9e2733e23c865c139bb52acae1b928218cc3797
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-06 20:37:29 +0000

    Change app.vue to use TimerList component and get timers from state

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <main>
-    <TimerCard
-      :title="'First Timer'"
-      :from="new Date(2022, 0, 3, 16, 10)"
-      :to="new Date(2022, 11, 31, 23, 59)"
-    />
+    <TimerList
+      :title="timerGroups[0].title"
+      :timers="timerGroups[0].timers" />
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import TimerCard from '@/components/TimerCard.vue'
+import { useStore } from './store'
+import TimerGroup from './types/timerGroup'
+import TimerList from '@/components/TimerList.vue'
 
 export default defineComponent({
   components: {
-    TimerCard,
+    TimerList
   },
-  setup() {},
+  setup() {
+    useStore().commit(
+      'createTimer',
+      {
+        timerTitle: 'First Timer',
+        from: new Date(2022, 0, 1, 0, 0),
+        to: new Date(2022, 11, 31, 23, 59),
+        groupTitle: 'First Group'
+      }
+    )
+  },
+  computed: {
+    timerGroups(): Array<TimerGroup> {
+      return useStore().state.user.timerGroups
+    }
+  }
 })
 </script>
 
