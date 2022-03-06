96c2e7f2f22a959348ecef1a95c7bce67d91d572
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-06 20:51:43 +0000

    Change timer and group names

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,10 @@ export default defineComponent({
     useStore().commit(
       'createTimer',
       {
-        timerTitle: 'First Timer',
+        timerTitle: 'New Years 2022!',
         from: new Date(2022, 0, 1, 0, 0),
         to: new Date(2022, 11, 31, 23, 59),
-        groupTitle: 'First Group'
+        groupTitle: 'Home'
       }
     )
   },
