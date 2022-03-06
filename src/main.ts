1d464fa3c68c9c2c6b4a657682a143d2612756dc
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-06 17:43:10 +0000

    Create store with addTimer method

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { store, key } from './store'
+
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas);
 
 createApp(App)
+  .use(store, key)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount('#app')
---
+++ src/store.ts
@@ -0,0 +1,53 @@
+import { InjectionKey } from 'vue'
+import { createStore, useStore as baseUseStore, Store } from 'vuex'
+import ColourTheme from './types/ColourTheme'
+import Timer from './types/Timer'
+import TimerGroup from './types/timerGroup'
+import User from './types/User'
+
+export interface State {
+  user: User
+}
+
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+  state() {
+    return {
+      user: {
+        timerGroups: [] as Array<TimerGroup>,
+        colourTheme: 'system' as ColourTheme
+      } as User,
+    }
+  },
+
+  mutations: {
+    // addTimer returns true if the timer was added to an existing group.
+    addTimer(state: State, { groupTitle, timerTitle, from, to }: { groupTitle: string, timerTitle: string, from: Date, to: Date}): void {
+      const newTimer: Timer = {
+        title: timerTitle,
+        from: from,
+        to: to
+      }
+
+      // Iterate over all of the user's timer groups until we see one with the right title, then add the new timer to it
+      for (let i = 0; i++; i < state.user.timerGroups.length) {
+        if (state.user.timerGroups[i].title === groupTitle) {
+          state.user.timerGroups[i].timers.push(newTimer)
+          return
+        }
+      }
+
+      // If there isn't a timer group with that name, create one with the new timer in it
+      state.user.timerGroups.push({
+        title: groupTitle,
+        timers: [newTimer]
+      })
+      return
+    },
+  },
+})
+
+export function useStore() {
+  return baseUseStore(key)
+}
