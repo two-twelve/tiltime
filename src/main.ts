fe1f9fd96f42a53a9f9c46daf5c689d08c149026
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-06 20:51:16 +0000

    Install extra icons

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { store, key } from './store'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas);
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, far, fab);
 
 createApp(App)
   .use(store, key)
