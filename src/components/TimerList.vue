51611ceba4c17a9500684c7c715875f4e7067866
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-06 20:37:12 +0000

    Create TimerList component

---
+++ src/components/TimerList.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+    <ul>
+      <TimerCard
+        v-for="timer of timers"
+        :key="timer.uuid"
+        :title="timer.title"
+        :from="timer.from"
+        :to="timer.to"
+      />
+    </ul>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import TimerCard from '@/components/TimerCard.vue'
+import Timer from '@/types/Timer'
+
+export default defineComponent({
+  components: {
+    TimerCard,
+  },
+  props: {
+    title: { type: String, required: true },
+    timers: { type: Array as PropType<Array<Timer>>, required: true }
+  },
+  setup() {}
+})
+</script>
