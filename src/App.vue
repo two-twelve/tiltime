61efca68e079ca184270d2898288528dc784a79b
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-06 22:01:18 +0000

    Add button to create new default 24 hour timer

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,38 @@
 <template>
   <main>
-    <TimersTab :timers="timerGroups[0].timers" />
+    <TimersTab :timers="store.state.user.timerGroups[0] ? store.state.user.timerGroups[0].timers : []" />
+    <div @click="addMockTimer">Add another timer</div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useStore } from '@/store'
-import TimerGroup from '@/types/timerGroup'
 import TimersTab from '@/components/TimersTab.vue'
 
 export default defineComponent({
   components: {
     TimersTab
   },
-  setup() {
-    useStore().commit(
-      'createTimer',
-      {
-        timerTitle: 'New Years 2022!',
-        from: new Date(2022, 0, 1, 0, 0),
-        to: new Date(2022, 11, 31, 23, 59),
-        groupTitle: 'Home'
-      }
-    )
-  },
-  computed: {
-    timerGroups(): Array<TimerGroup> {
-      return useStore().state.user.timerGroups
+  data() { return {
+    store: useStore(),
+  }},
+  methods: {
+    addMockTimer() {
+      const groupUUID = this.store.state.user.timerGroups[0] ? this.store.state.user.timerGroups[0].uuid : undefined
+      const from = new Date()
+      const to = new Date()
+      to.setDate(from.getDate() + 1)
+      this.store.commit(
+        'createTimer',
+        {
+          timerTitle: 'New Timer',
+          from: from,
+          to: to,
+          groupTitle: 'Home',
+          groupUUID: groupUUID
+        }
+      )
     }
   }
 })
