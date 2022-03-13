0cc572c2e1d6a50e4e72a32318e2b4673a828929
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-13 22:33:20 +0000

    Update TimersTabs to use store

--- src/components/TimersTabs.vue
+++ src/components/TimersTabs.vue
@@ -1,25 +1,72 @@
 <template>
-  <ul>
-    <li 
-        v-for="[i, timerGroup] of timerGroups.entries()" 
-        :key="timerGroup.uuid"
-        @click="$emit('setIndex',i)">
-      {{ timerGroup.title }}
-    </li>
-  </ul>
+  <nav>
+    <ol>
+      <li 
+          v-for="timerGroup of store.state.user.timerGroups" 
+          :key="timerGroup.uuid"
+          :class="timerGroup.uuid == store.state.activeTimerGroupUUID ? 'selected' : ''"
+          @click="setActiveTimerGroup(timerGroup.uuid)">
+        {{ timerGroup.title }}
+      </li>
+      <li>
+        <font-awesome-icon 
+          class="delete-icon" 
+          :icon="['fas', 'plus']"
+          @click="createNewTimerGroup" />
+      </li>
+    </ol>
+  </nav>
 </template>
 
 <script lang="ts">
-import TimerGroup from '@/types/timerGroup'
-import { defineComponent, PropType } from 'vue'
+import { defineComponent } from 'vue'
+import { useStore } from '@/store'
 
 export default defineComponent({
-  props: {
-    timerGroups: { type: Array as PropType<Array<TimerGroup>>, required: true }
-  },
-  emits: ['setIndex']
+  data() { return {
+    store: useStore()
+  }},
+  methods: {
+    setActiveTimerGroup(uuid: string) {
+      this.store.commit(
+        'setActiveTimerGroup',
+        {
+          timerGroupUUID: uuid
+        }
+      )
+    },
+    createNewTimerGroup() {
+      this.store.commit(
+        'createTimerGroup',
+        {
+          groupTitle: 'New Group'
+        }
+      )
+    }
+  }
 })
 </script>
 
 <style lang="scss" scoped>
+nav {
+  width:$app-width;
+  max-width: 100%;
+  border-color: lightgrey;
+  border-style: solid;
+  border-width: 0px 1px 1px 1px;
+  border-radius: 0px 0px 5px 5px;
+  padding: $spacer;
+  margin-bottom: $spacer;
+}
+ol {
+  display: flex;
+  flex-direction: row;
+}
+li {
+  margin: $spacer $spacer*2;
+  font-size: 125%;
+  &.selected {
+    text-decoration: underline;
+  }
+}
 </style>
