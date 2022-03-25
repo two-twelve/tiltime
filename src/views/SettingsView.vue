e963c5039b1c99a3283389cf727bfdfa3f898dae
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-25 20:41:48 +0000

    Style back link on settings view

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,5 +1,10 @@
 <template>
-  <router-link to="home">Back To My Timers</router-link>
+  <nav>
+    <router-link class="home-link" to="home">
+      <font-awesome-icon class="back-icon" :icon="['fas','arrow-circle-left']" />
+      Back To My Timers
+    </router-link>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -10,4 +15,16 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+nav {
+  width: $app-width;
+  max-width: 100%;
+  padding: $spacer*2 $spacer*4;
+  .home-link {
+    font-size: $font-size*1.2;
+    font-weight: $font-weight-bold;
+    .back-icon {
+      margin-right: $spacer * 0.5;
+    }
+  }
+}
 </style>
