72e2e46165cd88b1eff66f88150f79851d339cca
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-26 08:50:09 +0000

    Initial work on settings page

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -5,6 +5,63 @@
       Back To My Timers
     </router-link>
   </nav>
+  <article class="settings">
+    <h1 class="settings-title">
+      Settings
+      <font-awesome-icon class="icon" :icon="['fas','cogs']" />
+    </h1>
+    <section class="settings-group">
+      <h2 class="section-title">Push Notifications</h2>
+      <p>Give me a push notification:</p>
+      <ul>
+        <li>
+          <font-awesome-icon class="checkbox" :icon="['fas','check-circle']" />
+          When a timer ends
+        </li>
+        <li>
+          <font-awesome-icon class="checkbox" :icon="['fas','check-circle']" />
+          When a timer has one hour remaining
+        </li>
+        <li>
+          <font-awesome-icon class="checkbox" :icon="['far','circle']" />
+          The day before a timer ends
+        </li>
+      </ul>
+    </section>
+    <section class="settings-group">
+      <h2 class="section-title">Colour Themes</h2>
+      <ul>
+        <li>
+          <font-awesome-icon class="checkbox" :icon="['far','circle']" />
+          Light Theme
+        </li>
+        <li>
+          <font-awesome-icon class="checkbox" :icon="['far','circle']" />
+          Dark Theme
+        </li>
+        <li>
+          <font-awesome-icon class="checkbox" :icon="['fas','dot-circle']" />
+          System Theme
+        </li>
+      </ul>
+    </section>
+    <section class="settings-group">
+      <h2 class="section-title">Your Data</h2>
+      <button class="delete-data-button">
+        <font-awesome-icon class="icon" :icon="['fas','exclamation-circle']" />
+        Delete Your Data
+      </button>
+    </section>
+    <section class="about">
+      <h2 class="section-title">About</h2>
+      <p>
+        This app is open source! You can find it on 
+        <a class="github-link" href="">
+          GitHub<font-awesome-icon class="icon" :icon="['fab','github']" />
+        </a>
+      </p>
+    </section>
+  </article>
 </template>
 
 <script lang="ts">
@@ -24,6 +81,57 @@ nav {
     font-weight: $font-weight-bold;
     .back-icon {
       margin-right: $spacer * 0.5;
+    }
+  }
+}
+.settings {
+  width: $app-width * 0.75;
+  max-width: 100%;
+  padding: $spacer*3;
+  .settings-title {
+    margin: $spacer*1.5 0 $spacer*6 0;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    font-size: $font-size * 2;
+    font-weight: $font-weight-bold;
+    .icon {
+      margin-left: $spacer*1.5;
+      margin-bottom: $spacer;
+    }
+  }
+  .settings-group, .about {
+    margin-bottom: $spacer*6;
+    .section-title {
+      margin-bottom: $spacer*3;
+      font-size: $font-size * 1.4;
+      font-weight: $font-weight-bold;
+    }
+    p {
+      margin: $spacer;
+    }
+    ul {
+      margin: $spacer*2.5 $spacer;
+      li {
+        margin: $spacer $spacer*4;
+        .checkbox {
+          margin-right: $spacer * 1.5;
+        }
+      }
+    }
+    .delete-data-button {
+      margin: 0 $spacer;
+      color: $dark-negative;
+      .icon {
+        margin-right: $spacer * 0.5;
+        color: $dark-negative;     
+      }
+    }
+    .github-link {
+      text-decoration: underline;
+      .icon {
+        margin-left: $spacer/2
+      }
     }
   }
 }
