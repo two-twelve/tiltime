05a1866ff6820b6f3ceaa1cf8c1bedcad7d7e77d
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-27 16:21:39 +0100

    Move reset.scss to public/reset.css

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         additionalData: `
-        @import "./src/assets/styles/reset.scss";
         @import "./src/assets/styles/global.scss";
         `,
       },
