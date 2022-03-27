42ae5a7fdfa9baad8e04f6bd6de02df6cfde45d3
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-27 14:29:01 +0100

    Add notifications to user type

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,8 +1,10 @@
 import TimerGroup from './TimerGroup'
+import NotificationType from './NotificationType'
 import ColourTheme from './ColourTheme'
 
 type User = {
   timerGroups: Array<TimerGroup>,
+  notifications: Array<NotificationType>
   colourTheme: ColourTheme
 }
 
