a230e0c3c082ad690c54ee32f7e008b793e83e47
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-27 22:07:42 +0100

    Add PWA install button

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
     <div class="header-container">
       <header class="header">
         <h1 ref="title" class="title">TilTi.me</h1>
-        <router-link to="settings">
-          <font-awesome-icon class="user-icon" :icon="['fas', 'user-circle']" />
-        </router-link>
+        <nav class="button-group">
+          <font-awesome-icon 
+            v-if="installPromptEvent"
+            class="download-icon"
+            :icon="['fas','download']" 
+            @click="installPWA" />
+          <router-link to="settings">
+            <font-awesome-icon class="user-icon" :icon="['fas', 'user-circle']" />
+          </router-link>
+        </nav>
       </header>
     </div>
     <router-view />
@@ -18,6 +25,9 @@ import { annotate } from 'rough-notation'
 import { useRegisterSW } from "virtual:pwa-register/vue"
 
 export default defineComponent({
+  data(){return{
+    installPromptEvent: null as any
+  }},
   mounted() {
     const target = this.$refs.title as HTMLElement
     const titleHighlight = annotate(target, {
@@ -26,10 +36,25 @@ export default defineComponent({
       animate: false,
     })
     titleHighlight.show()
-
     const { needRefresh, updateServiceWorker } = useRegisterSW()
     if (needRefresh.value) {
       updateServiceWorker(false)
+    }
+    window.addEventListener(
+      'beforeinstallprompt', 
+      (event) => {
+        event.preventDefault()
+        this.installPromptEvent = event
+      }
+    )
+  },
+  methods: {
+    installPWA() {
+      this.installPromptEvent.prompt()
+      this.installPromptEvent.userChoice.then(
+        () => {
+          this.installPromptEvent = null
+      })
     }
   },
 })
@@ -64,9 +89,18 @@ main {
   font-size: $font-size * 2.5;
   font-weight: $font-weight-bold;
 }
-.user-icon {
-  padding: $spacer * 2;
-  font-size: $font-size * 2.5;
+.button-group {
+  display: flex;
+  align-items: center;
+  .download-icon, .user-icon {
+    padding: $spacer * 2;
+  }
+  .download-icon {
+    font-size: $font-size * 1.5;
+  }
+  .user-icon {
+    font-size: $font-size * 2.5;
+  }
 }
 .timers-tab {
   flex-grow: 1;
