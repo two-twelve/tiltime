2bb6a3731d7a1194319e130c464b3e283823d5b7
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-03-29 21:09:16 +0100

    Remove NewTimerForm import

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,11 @@
 import { defineComponent } from 'vue'
 import TimersTabs from '@/components/TimersTabs.vue'
 import TimersList from '@/components/TimersList.vue'
-import NewTimerForm from '@/components/NewTimerForm.vue'
 
 export default defineComponent({
   components: {
     TimersTabs,
-    TimersList,
-    NewTimerForm,
+    TimersList
   },
 })
 </script>
