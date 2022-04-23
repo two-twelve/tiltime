4819653cb170f7ed1f76a7bcd334279a07fcc99e
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-04-23 18:56:07 +0100

    Add more assertions to complete timer card test

--- src/components/TimerCard.spec.ts
+++ src/components/TimerCard.spec.ts
@@ -18,26 +18,44 @@ jest.mock('@/store', () => ({
   }),
 }));
 
-describe('TimerCard', () => {
-  it('should display timer title', () => {
-    const testTitle = 'Test Title'
-    const wrapper = shallowMount(
-      TimerCard,
-      {
-        props: {
-          uuid: '',
-          title: testTitle,
-          from: new Date(),
-          to: new Date()
-        },
-        global: {
-          components: {
-            'font-awesome-icon': FontAwesomeIcon
-          }
-        }
+describe('TimerCardComplete', () => {
+  const testTitle = 'Test Title'
+  const testFrom = new Date('10 Sep 1999 12:37:23')
+  const testTo = new Date('11 May 2000 20:10:37')
+  
+  const wrapper = shallowMount(
+    TimerCard,
+    {
+      props: {
+        uuid: '',
+        title: testTitle,
+        from: testFrom,
+        to: testTo
       },
-    )
+      global: {
+        components: {
+          'font-awesome-icon': FontAwesomeIcon
+        }
+      }
+    },
+  )
 
+  it('should display timer title', () => {
     expect(wrapper.find('input').element.value).toEqual(testTitle)
+  })
+  it('should display the correct start date', () => {
+    expect(wrapper.find(".start-container").element.textContent).toContain('10/09/99')
+  })
+  it('should display the correct end date', () => {
+    expect(wrapper.find(".end-container").element.textContent).toContain('11/05/00')
+  })
+  it('should display the correct start time', () => {
+    expect(wrapper.find(".start-container").element.textContent).toContain('12:37')
+  })
+  it('should display the correct end time', () => {
+    expect(wrapper.find(".end-container").element.textContent).toContain('20:10')
+  })
+  it('should display the correct percentage time elapsed', () => {
+    expect(wrapper.find(".progress-indicator").element.textContent).toEqual("100% elapsed ")
   })
 })
