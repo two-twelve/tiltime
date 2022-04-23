baf3895a584ca14c3c20b88f9e8ea8f7e5500be3
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-04-23 19:41:18 +0100

    Add tests for timer card not yet started

--- src/components/TimerCard.spec.ts
+++ src/components/TimerCard.spec.ts
@@ -19,7 +19,7 @@ jest.mock('@/store', () => ({
 }));
 
 describe('TimerCardComplete', () => {
-  const testTitle = 'Test Title'
+  const testTitle = 'This Timer\'s Finished!'
   const testFrom = new Date('10 Sep 1999 12:37:23')
   const testTo = new Date('11 May 2000 20:10:37')
   
@@ -60,5 +60,50 @@ describe('TimerCardComplete', () => {
   })
   it('should display the correct percentage time elapsed', () => {
     expect(wrapper.find(".progress-indicator").element.textContent).toEqual('100% elapsed ')
+  })
+})
+
+describe('TimerCardNotYetStarted', () => {
+  const testTitle = 'This Timer\'s Not Started!'
+  const testFrom = new Date('10 Sep 2999 21:13:56')
+  const testTo = new Date('11 May 3000 02:01:12')
+  
+  const wrapper = shallowMount(
+    TimerCard,
+    {
+      props: {
+        uuid: '',
+        title: testTitle,
+        from: testFrom,
+        to: testTo
+      },
+      global: {
+        components: {
+          'font-awesome-icon': FontAwesomeIcon
+        }
+      }
+    },
+  )
+
+  it('should display timer title', () => {
+    expect(wrapper.find('input').element.value).toEqual(testTitle)
+  })
+  it('should display the correct start date', () => {
+    expect(wrapper.find(".start-container").element.textContent).toContain('10/09/99')
+  })
+  it('should display the correct end date', () => {
+    expect(wrapper.find(".end-container").element.textContent).toContain('11/05/00')
+  })
+  it('should display the correct start time', () => {
+    expect(wrapper.find(".start-container").element.textContent).toContain('21:13')
+  })
+  it('should display the correct end time', () => {
+    expect(wrapper.find(".end-container").element.textContent).toContain('02:01')
+  })
+  it('should display 0 hours, 0 minutes and 0 seconds remaining', () => {
+    expect(wrapper.find(".countdown").element.textContent).toEqual('242 days,\n4 hours, 47 minutes\nand 16 seconds.')
+  })
+  it('should display the correct percentage time elapsed', () => {
+    expect(wrapper.find(".progress-indicator").element.textContent).toEqual('0% elapsed ')
   })
 })
